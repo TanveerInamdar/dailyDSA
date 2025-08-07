a652ad09ca6f320933a2ac9cc650dba53b06ba9d
Tanveer Inamdar <tinamdar@ttu.edu>
2025-08-06 20:50:13 -0500

    6th August

---
+++ Leetcode38.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        s = self.countAndSay(n - 1)
+
+        res = []
+
+        i = 0
+        while i < len(s):
+            count = 1
+            while i + 1 < len(s) and s[i] == s[i + 1]:
+                i += 1
+                count += 1
+
+            res.append(str(count))
+            res.append(s[i])
+            i += 1
+        return ''.join(res)
+
